c53ef0675feabf44d461b15c8b0fc3f15397eb0b
mc <zjueman@qq.com>
2020-02-09 21:59:30 +0800

    threading

--- threading/p5.py
+++ threading/p5.py
@@ -1,22 +1,22 @@
 from time import sleep
 from threading import Thread
 
-count = 0
+#小明要下载10个视频
+tasks = ['movie1', 'movie2', 'movie3','movie4','movie5','movie6','movie7','movie8','movie9', 'movie10']
 
-def add():
-	global count
-	for i in range(10000):
-		count += 1
-		#print(count)
+def download(movie):
+	print(f'start downloading {movie}')
+	sleep(1) #模拟下载电影需要的时间
+	print(f'finished download {movie}')
 
 threads = []
-for i in range(5):
-	t = Thread(target=add)
+for task in tasks:
+	t = Thread(target=download, args=(task,)) #创建线程
+	t.start() #启动线程
 	threads.append(t)
-	t.start()
 
 for t in threads:
-	t.join()
+	t.join() #当前线程等待线程t执行完成后再执行后面的代码
 
-print(f"final: {count}")
-
+#都下载完成后，能够打印一句话
+print("都下载完了!")
